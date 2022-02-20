dbf789c720e402a69b33be2762aaf1d6efd2c585
kyj91032 <83624600+kyj91032@users.noreply.github.com>
2022-02-20 18:25:26 +0900

    Update prime number.py

--- subalgorithm/prime number.py
+++ subalgorithm/prime number.py
@@ -2,7 +2,36 @@
 
 # 소수 판별 기본 동작 과정
 '''
-1. 
+1. 2부터 sqrt(n)까지 약수가 존재하는지(n % i == 0) 판별
+'''
+
+''' python
+
+import math
+
+def is_prime_number(x):
+    for i in range(2, int(math.sqrt(x)) + 1): 
+        if x % i == 0:
+            return False
+    return True
 
 '''
 
+''' n까지 모든 자연수를 소수 판별할 때 아이디어: 에라토스테네스의 체
+
+1. 소수 판별 테이블 True로 초기화
+2. 테이블에 남은 수 중 첫번째 수의 배수를 지운다.
+- 반복
+
+import math
+
+n = 1000 # 2부터 1,000까지의 모든 수에 대하여 소수 판별
+array = [True for i in range(n + 1)] # 소수 판별 테이블 True로 초기화. 여기서 소수가 아닌 수를 지워나갈 것.
+
+for i in range(2, int(math.sqrt(n)) + 1): # 2부터 n의 제곱근까지의 모든 수를 확인하며 (제곱근 이전의 배수를 모두 확인하였으므로 제곱근 이후부터 n까지는 테이블이 확정된다)
+    if array[i] == True: # i가 소수인 경우 (남은 수인 경우) (소수가 남게 됨)
+        j = 2 # i를 제외한 i의 모든 배수를 지우기
+        while i * j <= n:
+            array[i * j] = False
+            j += 1
+'''
