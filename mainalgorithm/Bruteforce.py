eb1469dd26063319f81476c2597b36c027184b0c
kyj91032 <83624600+kyj91032@users.noreply.github.com>
2022-02-20 23:54:47 +0900

    Update Bruteforce.py

--- mainalgorithm/Bruteforce.py
+++ mainalgorithm/Bruteforce.py
@@ -17,3 +17,62 @@ for i in range(n + 1):
 print(cnt)
 
 '''
+
+''' python; 3085 사탕게임
+
+import sys
+input = sys.stdin.readline
+
+def check(arr): # '연속되는 숫자의 최댓값 확인하는 코드' 따로 떼서 함수로 만들기. 이렇게 쪼개서 생각할 수 있어야 함. 사실상 2차원 선형탐색.
+    n = len(arr)
+    max = 1
+
+    for i in range(n):
+        cnt = 1
+        for j in range(1, n): # 가로 전체 탐색
+            if arr[i][j] == arr[i][j-1]:
+                cnt += 1
+            else:
+                cnt = 1
+            if cnt > max: # 매번 최댓값 갱신
+                max = cnt
+        cnt = 1
+        for j in range(1, n): # 세로 전체 탐색
+            if arr[j][i] == arr[j-1][i]:
+                cnt += 1
+            else:
+                cnt = 1
+            if cnt > max:
+                max = cnt
+    return max
+
+
+n = int(input())
+arr = [list(input()) for _ in range(n)] # 이중 리스트로 바로 입력받기
+answer = 0
+
+for i in range(n):
+    for j in range(n):
+        if j + 1 < n:
+            arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
+            
+            temp = check(arr)
+
+            if temp > answer:
+                answer = temp
+               
+            arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
+
+        if i + 1 < n:
+            arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
+            
+            temp = check(arr)
+
+            if temp > answer:
+                answer = temp
+            
+            arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
+            
+print(answer)
+
+'''
