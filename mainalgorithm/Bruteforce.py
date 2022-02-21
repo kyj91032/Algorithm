9b2af578336956e74e1546dac0ac38653f183940
kyj91032 <83624600+kyj91032@users.noreply.github.com>
2022-02-21 23:46:37 +0900

    Update Bruteforce.py

--- mainalgorithm/Bruteforce.py
+++ mainalgorithm/Bruteforce.py
@@ -2,7 +2,7 @@
 
 # 완전 탐색의 기본 동작 과정
 '''
-1. 처음부터 끝까지 모두 확인한다. (선형탐색, 2차원 선형탐색, 
+1. 처음부터 끝까지 모두 확인한다. (1 ~ 3차원 선형탐색, 최대 최소 선형탐색, 
 '''
 
 ''' python
@@ -76,3 +76,40 @@ for i in range(n):
 print(answer)
 
 '''
+
+''' python; 1476 날짜 계산
+
+a, b, c = map(int, input().split())
+y = 1 # 연도 자체(답)를 1부터 선형 탐색함. 답이 y라고 하면 맞나?를 계속 확인
+
+while True:
+    if (y - a) % 15 == 0 and (y - b) % 28 == 0 and (y - c) % 19 == 0:
+        print(y)
+        break
+    y += 1
+
+'''
+
+''' python; 1107 리모컨
+
+import sys
+input = sys.stdin.readline
+target = int(input())
+n = int(input())
+broken = list(map(int, input().split()))
+
+min_count = abs(100 - target) # 현재 채널에서 + 혹은 -만 사용하여 이동하는 경우. 최댓값을 초기값으로 설정.
+
+for nums in range(1000001): # 고장난 채널을 제외한 모든 채널 탐색
+    nums = str(nums)
+    
+    for j in range(len(nums)):
+        if int(nums[j]) in broken:
+            break
+        elif j == len(nums) - 1:
+            min_count = min(min_count, abs(int(nums) - target) + len(nums)) # 고장난 숫자 없이 마지막 자리까지 왔다면 min_count 비교 후 업데이트
+
+print(min_count)
+
+'''
+
