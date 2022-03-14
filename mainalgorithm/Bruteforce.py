dbb22f3a546f9a159fe292440f10d489c067a2a3
kyj91032 <83624600+kyj91032@users.noreply.github.com>
2022-03-14 17:55:34 +0900

    Update Bruteforce.py

--- mainalgorithm/Bruteforce.py
+++ mainalgorithm/Bruteforce.py
@@ -234,7 +234,7 @@ print(result)
 '''
 
 
-''' 완전 탐색의 재귀적 구현 recursive bruteforce
+''' n과 m (3) 완전 탐색의 재귀적 구현 recursive bruteforce
 
 n, m = list(map(int, input().split()))
  
@@ -250,4 +250,29 @@ def rec(): # bruteforce 인데 반복으로 안되서 재귀로 접근.
             rec() # 재귀 호출
             s.pop()
 rec()
+'''
+
+
+''' 9095 1,2,3 더하기
+
+n = int(input())
+
+def sums(n):
+    if n == 1:
+        return(1)
+    elif n == 2:
+        return(2)
+    elif n == 3:
+        return(4)
+    else:
+        return sums(n-1) + sums(n-2) + sums(n-3)
+        
+for i in range(n):
+    a = int(input())
+    print(sums(a))
+'''
+
+
+'''
+
 '''
