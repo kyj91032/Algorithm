0fad42e383fb3581d3b142bffe4ab8743345d619
kyj91032 <83624600+kyj91032@users.noreply.github.com>
2022-04-02 20:51:50 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -13,3 +13,43 @@ while 1:
 			i += 1
 	else:
 		i += 1
+
+
+T = int(input())
+
+
+for i in range(1, T + 1):
+	print("Case #%d: " % i, end = '')
+	
+	n = int(input())
+	
+	graph = [0]
+	
+	fl = list(map(int, input().split()))
+	pl = list(map(int, input().split()))
+	
+	for j in range(1, n + 1):
+		graph.append((pl[i - 1], fl[i - 1]))
+	
+	visited = [False] * (n + 1)
+	
+	itr = []
+	for j in range(n + 1):
+		if j not in pl:
+			itr.append(j)
+	
+	sum = []
+	
+	tmp = 0
+	
+	def dfs(v):
+		visited[v] = True
+		
+		if v not in pl:
+			sum.append(tmp)
+		for k in range(n):
+			if pl[k] == v:
+				if not visited[k + 1]:
+					dfs(k + 1)
+		
+	
