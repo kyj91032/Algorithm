41c8f07a2f6bec2f52c6a5eae31ca4ea1d8dd0ac
kyj91032 <83624600+kyj91032@users.noreply.github.com>
2022-04-12 22:00:02 +0900

    Update Backtracking.py

--- mainalgorithm/Backtracking.py
+++ mainalgorithm/Backtracking.py
@@ -472,3 +472,30 @@ def rec(): # 순회 경로 재귀로 구현
 rec()
 print(res)
 '''
+
+
+''' 6603 로또 - 리스트에서 오름차순 순열
+
+q = []
+def rec(start):
+	if len(q) == 6: # 6개 오름차순 순열 출력
+		print(' '.join(map(str, q)))
+		return
+	
+	for i in s[start:]:
+		if i not in q:
+			q.append(i)
+			rec(s.index(i) + 1)
+			q.pop()
+
+while 1:
+	a = list(map(int, input().split()))
+	if a == [0]:
+		break
+	
+	k = a[0]
+	s = a[1:]
+	
+	rec(0)
+	print()
+'''
