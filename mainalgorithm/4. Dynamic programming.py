35015030cb9969668a5bdc4c9a6b3c5a0832c565
kyj91032 <83624600+kyj91032@users.noreply.github.com>
2022-07-14 23:00:32 +0900

    Update 4. Dynamic programming.py

--- mainalgorithm/4. Dynamic programming.py
+++ mainalgorithm/4. Dynamic programming.py
@@ -464,7 +464,7 @@ array = list(map(int, input().split()))
 d = [1] * n
 d[0] = array[0]
 for i in range(1, n):
-  for j in range(i): # 일차원 반복문으로 코드를 짜면, 앞 쪽이 고려되지 알 수 있는데, 이 때 n의 크기를 보고 이중 반복문을 활용할 생각을 해야함.
+  for j in range(i): # 일차원 반복문으로 코드를 짜면, 앞 쪽이 고려되지 않는 걸 알 수 있는데, 이 때 n의 크기를 보고 이중 반복문을 활용할 생각을 해야함.
     if array[j] < array[i]:
       d[i] = max(d[i], d[j] + array[i]) # ai가 더 크면 이전 d에 넣어보고
     else:
