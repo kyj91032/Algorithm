d029cf68171da77bede0922e3600b83eb4511538
kyj91032 <83624600+kyj91032@users.noreply.github.com>
2022-07-24 19:14:51 +0900

    Update Depth-First Search.py

--- mainalgorithm/Depth-First Search.py
+++ mainalgorithm/Depth-First Search.py
@@ -1,4 +1,4 @@
-# 깊이 우선 탐색(DFS): 가중치가 1인 그래프에서, 탐색하지 않은 인접 노드를 우선으로 최대한 깊숙이 탐색하여 노드를 방문한 후, 다시 돌아가 다른 경로로 탐색하는 방법이다.
+# 깊이 우선 탐색(DFS): 가중치가 1인 그래프에서, 탐색하지 않은 인접 노드를 우선으로 최대한 깊숙이 탐색하여 노드를 방문한 후, 다시 돌아가 다른 경로로 탐색하는 방법이다. 백트래킹의 일종이라고 할 수 있다. 그래프에서의 백트래킹
 
 # DFS의 기본 동작 과정
 
@@ -69,3 +69,48 @@ for i in range(n):
 print(result)
 
 
+# 13023 ABCDE - 그래프임은 확실, 깊이가 5이상인지 판단하는 것 => dfs. 모든 경우를 탐색해야 하므로, 인접노드를 다 확인한 노드는 자취를 지움.
+
+import sys
+sys.setrecursionlimit(10**6)
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n)]
+
+for i in range(m): # 순회하며 확인해야 하므로, 인접 리스트 방식으로 그래프 저장
+	tmp = list(map(int, input().split()))
+	for j in range(n):
+		if tmp[0] == j:
+			graph[j].append(tmp[1])
+			graph[tmp[1]].append(j)
+			break
+visited = [0] * n
+s = []
+m = 0
+
+def dfs(graph, v, visited):
+	visited[v] = 1
+	s.append(v)
+	global m # 전역변수 m 선언. 외부 m과 연결됨. s는 리스트라서 재할당시에만 global 필요함
+	m = max(m, len(s))
+	if m >= 5:
+		for p in range(n):
+			visited[p] = 1
+		return
+	for k in graph[v]:
+		if visited[k] == 0:
+			dfs(graph, k, visited)
+	s.pop()
+	visited[v] = 0 # dfs 중에 자취(visited)를 지워가며 dfs를 한다. 모든 경우를 순회하기 위해서. 지우는 타이밍은 pop()을 할 때와 동일.
+
+for i in range(n):
+	dfs(graph, i, visited)
+	if m >= 5:
+		print(1)
+		break
+	visited = [0] * n
+	
+
+if m <= 4:
+	print(0)
