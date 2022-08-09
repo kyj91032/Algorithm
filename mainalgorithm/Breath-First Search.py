f06792501a93be13b94078c737ea138ddfa87b97
kyj91032 <sees77770@gmail.com>
2022-08-09 16:05:22 +0900

    Update Breath-First Search.py

--- mainalgorithm/Breath-First Search.py
+++ mainalgorithm/Breath-First Search.py
@@ -125,7 +125,7 @@ res = 0
 for i in range(n):
     for j in range(m):
         if matrix[i][j] == 1:
-            queue.append([i, j]) # 시작노드 미리 넣어두고 밑에서 bfs 시작
+            queue.append([i, j]) # 시작노드 미리 넣어두고 밑에서 bfs 시작. 그러면 여러 시작점에서 번갈아가며 bfs함
 
 def bfs():
     while queue:
@@ -324,4 +324,38 @@ def bfs(start, end):
 			break
 bfs(n, k)
 
+
+# 1261 알고스팟 - bfs에서 조건에 따라 우선적으로 실행하는 방법: queue.appendleft()하면 됨, 격자그래프에서 visited 사용
+
+from collections import deque
+
+n, m = map(int, input().split())
+
+graph = [list(map(int, input())) for _ in range(m)]
+visited = [[-1] * n for _ in range(m)] # 격자그래프지만 graph의 값이 따로 정보를 갖고있다면 visited를 따로 써야함
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+	queue = deque()
+	queue.append((x, y))
+	visited[x][y] = 0
+	while queue:
+		x, y = queue.popleft()
+		for i in range(4):
+			nx = x + dx[i]
+			ny = y + dy[i]
+			if nx >= m or nx < 0 or ny < 0 or ny >= n:
+				continue
+			if visited[nx][ny] != -1:
+				continue
+			if graph[nx][ny] == 0:
+				visited[nx][ny] = visited[x][y]
+				queue.appendleft((nx, ny)) # 벽 없는곳 먼저 뚫기
+			if graph[nx][ny] == 1:
+				visited[nx][ny] = visited[x][y] + 1
+				queue.append((nx, ny))
+	print(visited[m-1][n-1])
+bfs(0, 0)
 
