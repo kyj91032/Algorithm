efcd357b49229d64e36fe00a3737a1738cf23b92
kyj91032 <sees77770@gmail.com>
2022-08-12 15:26:35 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,100 +1,40 @@
-a, b, c = map(int, input().split())
+import sys
 
-i = 0
-while 1:
-	y1 = a + 15 * i
-	y2 = (y1 - b) / 28
-	if y2 % 1 == 0 and y2 > 0:
-		y3 = (y2 - c) / 19
-		if y3 % 1 == 0 and y3 > 0:
-			print(int(y3))
-			break
-		else:
-			i += 1
-	else:
-		i += 1
+input = sys.stdin.readline
 
+n, m, r = map(int, input().split())
 
-T = int(input())
+a = []
 
-def dfs(v):
-	global tmp
-	global sl
-	if not visited[v]:
-		visited[v] = True
-		tmp += fi[v - 1]
-		for i in graph[v]:
-			dfs(i)
-	sl.append((tmp, v))
-	tmp -= fi[v - 1]
+for i in range(n):
+	for j in range(m):
+		a.append(list(map(int, input().split())))
 
-def dfs2(v):
-	global m
-	if v == 0:
-		return
-	if visited2[v] == True:
-		return
-	visited2[v] = True
-	m = max(m, fi[v - 1])
-	dfs2(pi[v - 1])
+r = list(map(int, input().split()))
 
-for i in range(1, T + 1):
-	print("Case #%d: " % i, end = '')
-	
-	n = int(input())
-	
-	fi = list(map(int, input().split()))
-	pi = list(map(int, input().split()))
-	
-	graph = [[] for _ in range(n + 1)]
-	
-	visited = [False] * (n + 1)
-	visited2 = [False] * (n + 1)
-	
-	for a in range(n + 1):
-		for b in range(n):
-			if pi[b] == a:
-				graph[a].append(b + 1)
-	
-	tmp = 0
-	sl = []
-	
-	ini = []
-	for j in range(n + 1):
-		if len(graph[j]) == 0:
-			ini.append(j)
-	
-	p0 = []
-	for j in range(n):
-		if pi[j] == 0:
-			p0.append(j + 1)
-	
-	result = 0
-	
-	for q in p0:
+for i in r:
+	if r == 1:
+		a.reverse()
+	if r == 2:
+		for j in range(n):
+			a[j].reverse()
+	if r == 3:
+		tmp = [[] for _ in range(m)]
+		for j in range(m):
+			for k in range(n):
+				tmp[j].append(a[n-1-k][j])
+		a = tmp
+		tmp = n
+		n = m
+		m = tmp
+	if r == 4:
+		tmp = [[] for _ in range(m)]
+		for j in range(m):
+			for k in range(n):
+				tmp[j].append(a[k][m-1-j])
+		a = tmp
+		tmp = n
+		n = m
+		m = tmp
+	if r == 5:
 		
-		dfs(q)
-
-		isl = []
-		l = len(sl)
-		for j in range(l):
-			if sl[j][1] in ini:
-				isl.append(sl[j])
-		
-		isls = sorted(isl, key = lambda x : x[0])
-		
-		islsi = []
-		for j in range(len(isls)):
-			islsi.append(isls[j][1])	
-
-		m = 0
-		M = 0
-	
-		for u in islsi:
-			m = 0
-			dfs2(u)
-			M += m
-	
-		result += M
-		
-	print(result)
