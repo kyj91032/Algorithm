368b27d68222ecb9d9709ee5f5c8d8ccb5ff1a45
kyj91032 <sees77770@gmail.com>
2022-09-13 23:12:13 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,40 +1,20 @@
-import sys
+n = int(input())
 
-input = sys.stdin.readline
+cuv = [list(map(int, input().split())) for _ in range(n)]
 
-n, m, r = map(int, input().split())
+visited = [[0] * 101 for _ in range(101)]
+temp = [[0] * 101 for _ in range(101)]
 
-a = []
+dx = [1, 0, -1, 0]
+dy = [0, -1, 0, 1]
 
-for i in range(n):
-	for j in range(m):
-		a.append(list(map(int, input().split())))
-
-r = list(map(int, input().split()))
-
-for i in r:
-	if r == 1:
-		a.reverse()
-	if r == 2:
-		for j in range(n):
-			a[j].reverse()
-	if r == 3:
-		tmp = [[] for _ in range(m)]
-		for j in range(m):
-			for k in range(n):
-				tmp[j].append(a[n-1-k][j])
-		a = tmp
-		tmp = n
-		n = m
-		m = tmp
-	if r == 4:
-		tmp = [[] for _ in range(m)]
-		for j in range(m):
-			for k in range(n):
-				tmp[j].append(a[k][m-1-j])
-		a = tmp
-		tmp = n
-		n = m
-		m = tmp
-	if r == 5:
+for c in cuv:
+	x = c[0]
+	y = c[1]
+	d = c[2]
+	g = c[3]
+	temp[x][y] = 1
+	temp[x+dx[d]][y+dy[d]] = 1
+	
+	for i in range(g - 1):
 		
