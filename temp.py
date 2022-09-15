2b33d362f712500c8f5e2074ab0d107a516e2ce9
kyj91032 <sees77770@gmail.com>
2022-09-15 16:40:11 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -20,7 +20,7 @@ for c in cuv:
 			if temp[i][j] == 1 and visited[i][j] == 0:
 				visited[i][j] = 1
 	temp = [[0] * 101 for _ in range(101)]
-	# 끝점 잡기
+	# 끝점 잡기 모든 점 확인하며 visit1이면서 거리 가장 먼 좌표
 	# 회전함수
+	# 거리리스트 미리 만들기??
 	for i in range(g - 1):
-		
