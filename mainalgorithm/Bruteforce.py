1324f1a7b4ab062a8bbc37ebc497c676f5655122
kyj91032 <sees77770@gmail.com>
2022-09-19 12:06:58 +0900

    Update Bruteforce.py

--- mainalgorithm/Bruteforce.py
+++ mainalgorithm/Bruteforce.py
@@ -4,6 +4,7 @@
 
 1. 처음부터 끝까지 모두 확인(탐색)한다. for (반복과 1 ~ 3차원 선형탐색, 재귀와 깊이 조건, 최대최소 선형탐색, 좌표 이동경로 정의 후 탐색 or 시뮬레이션, 함수형 프로그래밍, 비트마스크)
 2. 주어진 조건을 제외하고, 불필요한 탐색을 줄일 가능성이 있는지 알아본다. if (탐색의 범위축소 ex) += 1 대신 += k, 백트래킹)
+3. 분류해서 부분 문제를 해결한다. def
 + 예시(최소한 처음중간끝이 나뉘도록)를 가지고 먼저 구현한 뒤 일반화 해나가기
 
 
@@ -925,4 +926,55 @@ for i in range(2*size+3):
     for number in numbers:
         print(number_format[int(number)][i]+' ', end='')
     print('')
+
+
+
+# 16931 겉넓이 구하기 - 분류해서 해결하기. 6면의 겉넓이를 나눠서 
+
+n, m = map(int, input().split())
+
+a = []
+
+for _ in range(n):
+	a.append(list(map(int, input().split())))
+
+b = n * m
+t = n * m
+
+no = 0
+e = 0
+w = 0
+s = 0
+
+for i in range(m):
+	no += a[0][i]
+	s += a[n-1][i]
+
+for i in range(n):
+	e += a[i][m-1]
+	w += a[i][0]
+
+for i in range(n-1):
+	for j in range(m):
+		if a[i][j] < a[i+1][j]:
+			no += a[i+1][j] - a[i][j]
+
+for i in range(n-1):
+	for j in range(m):
+		if a[n-i-1][j] < a[n-i-2][j]:
+			s += a[n-i-2][j] - a[n-i-1][j]
+
+for i in range(n):
+	for j in range(m-1):
+		if a[i][j] < a[i][j+1]:
+			w += a[i][j+1] - a[i][j]
+
+
+for i in range(n):
+	for j in range(m-1):
+		if a[i][m-j-1] < a[i][m-j-2]:
+			e += a[i][m-2-j] - a[i][m-1-j]
+
+print(no + e + w + s + t + b)
 '''
+
