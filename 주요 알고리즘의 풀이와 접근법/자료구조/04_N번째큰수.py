d9d932fdfeb3c4322e0b73a961e77ba6f2008838
Yeongjae Kim <sees77770@gmail.com>
2024-04-04 14:16:03 +0900

    04_우선순위큐

---
+++ 주요 알고리즘의 풀이와 접근법/자료구조/04_N번째큰수.py
@@ -0,0 +1,40 @@
+# https://boj-helper.vercel.app/solve/2075?language=python
+
+# 생각 과정
+'''
+처음에 두가지 풀이를 시도했다.
+첫번째는 리스트에서 정렬하고 5번째 인덱스를 출력하는 방법이었다. 시간복잡도 O(nlogn)
+두번째는 우선순위 큐를 사용해서 5번째 큰 수를 출력하는 방법이었다. 시간복잡도 O(nlogn)
+사실 둘이 똑같네 .. 
+-> 둘다 메모리 초과가 났다.
+
+=> 우선순위 큐의 크기를 정하고 (고정시키고) 원소들을 돌린다.
+=> 우선순위 큐가 최대, 최소를 구하는데에 최적화된 역할 + 큐로서의 역할을 모두 수행한다.
+'''
+
+# 풀이 코드
+import heapq
+
+n = int(input())
+
+pq = [] # 크기를 n으로 고정. n번째 큰 수니까 마지막에 큐에서 pop하면(그 큐 안에서의 최솟값) n번째 큰 수가 나오도록 함. => 메모리 초과 해결
+
+for i in range(n):
+    tmp = list(map(int, input().split()))
+    for e in tmp:
+        if len(pq) == n: # 큐의 크기가 n이면
+            if e > pq[0]:
+                heapq.heappop(pq)
+                heapq.heappush(pq, e)
+        else:
+            heapq.heappush(pq, e)
+
+print(pq[0])
+
+
+# 피드백 후 정리
+'''
+1. 우선순위 큐는 리스트를 선언하고, 그 리스트에 힙 연산(heappush, heappop)을 수행함으로써 구현하는 것이다.
+   그렇기 때문에 리스트는 계속 정렬 상태를 유지하게 되고 pq[0]은 최솟값을 가르키게 된다.
+2. 우선순위 큐도 결국 큐이기 때문에 크기를 조절하여 적절히 push와 pop을 할 수 있어야 한다.
+'''
--- 주요 알고리즘의 풀이와 접근법/자료구조/우선순위 큐/03_최대힙.py
+++ 주요 알고리즘의 풀이와 접근법/자료구조/우선순위 큐/03_최대힙.py
@@ -26,6 +26,30 @@ for _ in range(n):
             print(0)
 
 
+# 절댓값 힙
+import sys
+import heapq
+
+def input(): return sys.stdin.readline().rstrip()
+
+pq = []
+
+n = int(input())
+
+for i in range(n):
+    k = int(input())
+    if k == 0:
+        if pq:
+            print(heapq.heappop(pq)[1])
+        else:
+            print(0)
+    elif k < 0:
+        heapq.heappush(pq, (-k, k)) # 처음엔 절댓값을 기준으로 pop하고, 그 다음엔 원래 값을 기준으로 pop하기 위해 
+        # (절댓값, 원래값) 순서로 튜플로 넣어줌. 그리고 출력할땐 원래값 ([1])을 출력함.
+    else:
+        heapq.heappush(pq, (k, k))
+
+
 # 피드백 후 정리
 '''
 1. 파이썬에서 우선순위 큐는 heapq를 사용한다.
@@ -40,4 +64,7 @@ for _ in range(n):
 2. 리스트에서 최댓값을 찾는 것과 우선순위 큐에서 최댓값을 찾는 것의 시간복잡도 차이
     리스트: O(n)을 반복
     우선순위 큐: O(logn)을 반복
+
+3. 우선순위 큐는 튜플의 첫번째 값, 두번째 값, 순서대로 정렬된다.
+
 '''
