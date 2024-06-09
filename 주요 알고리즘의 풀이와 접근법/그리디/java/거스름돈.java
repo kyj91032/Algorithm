aa9eda2a36e191befb9b817a9100253fde7d0a45
Yeongjae Kim <sees77770@gmail.com>
2024-06-09 19:10:48 +0900

    자바 init

---
+++ 주요 알고리즘의 풀이와 접근법/그리디/java/거스름돈.java
@@ -0,0 +1,25 @@
+package java;
+// https://boj-helper.vercel.app/solve/14916?language=python
+
+// 생각 과정
+/*
+ * 
+ * 
+ */
+
+// 풀이 코드
+
+public class 거스름돈 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+
+
+// 피드백 후 정리
+
+/*
+ * 
+ * 
+ */
