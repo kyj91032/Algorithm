39d2ae6de9a52cf05ffeb70155735e71d4a25d12
Yeongjae Kim <sees77770@gmail.com>
2024-07-03 10:25:52 +0900

    자바 test

---
+++ 주요 알고리즘의 풀이와 접근법/그리디/거스름돈_00.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 생각 과정
+/*
+ * 
+ * 
+ */
+
+// 풀이 코드
+
+
+public class 거스름돈_00 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int a = Integer.parseInt(input);
+
+        System.out.println(a);
+        
+    }
+}
+
+// 피드백 후 정리
+
+/*
+ * 
+ * 
+ */
